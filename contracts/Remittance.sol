590776eb402bd37ddfc60da30ca4bd488faaf1a3
zhunchong87 <zhunchong87@gmail.com>
2018-03-22 21:54:16 +0800

    Updated contract code. Added refund(). Modified withdraw(). Simplify RemitStruct type.

--- contracts/Remittance.sol
+++ contracts/Remittance.sol
@@ -4,15 +4,14 @@ contract Remittance{
 	mapping(bytes32 => RemitStruct) public remittances;
 	uint constant DURATION_LIMIT = 10;
 
-	event LogDeposit(address indexed sender, address indexed receiver, uint amount, uint deadline);
-	event LogWithdraw(address indexed withdrawer, uint amount, uint deadline);
+	event LogDeposit(address indexed sender, address indexed receiver, uint amount, uint deadline, bytes32 secret);
+	event LogWithdraw(address indexed withdrawer, uint amount, uint deadline, bytes32 secret);
+	event LogRefund(address indexed sender, uint amount, uint deadline, bytes32 secret);
 
 	struct RemitStruct{
-		address remitOwner;
-		address remitReceiver;
-		uint remitAmt;
+		address remitSender;
+		uint remitBalance;
 		uint deadline;
-		bool hasWithdrawn;
 	}
 
 	function Remittance() public{
@@ -31,7 +30,7 @@ contract Remittance{
 			with the exchange, waiting to be withdrawn by the receiver.
 			In this case, the deposit should be rejected so that the receiver is able to have multiple pending remittance at once.
 		*/  
-		require(remittances[secret].remitOwner == address(0));
+		require(remittances[secret].remitSender == address(0));
 
 		// Validate basic input
 		require(receiver != address(0));
@@ -40,43 +39,67 @@ contract Remittance{
 
 		// Store remittance
 		RemitStruct memory curRemittance;
-		curRemittance.remitOwner = msg.sender;
-		curRemittance.remitReceiver = receiver;
-		curRemittance.remitAmt = msg.value;
+		curRemittance.remitSender = msg.sender;
+		curRemittance.remitBalance = msg.value;
 		curRemittance.deadline = block.number + duration;
-		curRemittance.hasWithdrawn = false;
 
 		remittances[secret] = curRemittance;
-		LogDeposit(msg.sender, receiver, msg.value, curRemittance.deadline);
+		LogDeposit(msg.sender, receiver, msg.value, curRemittance.deadline, secret);
 	}
 
 	/*
 		Withdraw the balance by providing the secret passwords.
 	*/
-	function withdraw(address secretKey, bytes32 secret)
+	function withdraw(bytes32 secret)
 		public
 	{
 		// Retrieve remittance
-		bytes32 realSecret = keccak256(secretKey, secret);
+		bytes32 realSecret = keccak256(msg.sender, secret);
 		RemitStruct memory _remittance = remittances[realSecret];
 
 		// Validate if realSecret is correct
-		require(_remittance.remitOwner != address(0));
+		require(_remittance.remitSender != address(0));
 
 		// Validate if remittance has already been withdrawn
-		require(_remittance.hasWithdrawn == false);
+		require(_remittance.remitBalance > 0);
 
 		// Validate that current block has not exceed deadline.
-		// Only remittance owner is able to withdraw the funds back after deadline has exceeded.
-		require((block.number <= _remittance.deadline && msg.sender == _remittance.remitReceiver) || msg.sender == _remittance.remitOwner);
+		require(block.number <= _remittance.deadline);
 
 		// Soft delete.
-		remittances[realSecret].hasWithdrawn = true;
-		LogWithdraw(msg.sender, _remittance.remitAmt, _remittance.deadline);
+		remittances[realSecret].remitBalance = 0;
+		LogWithdraw(msg.sender, _remittance.remitBalance, _remittance.deadline, realSecret);
 
 		// Interact with untrusted address last.
-		msg.sender.transfer(_remittance.remitAmt);
+		msg.sender.transfer(_remittance.remitBalance);
 	}
+
+	/*
+		Allow refund for remittance sender if the deadline has passed.
+	*/
+	function refund(bytes32 secret)
+		public
+	{
+		// Retrieve remittance
+		RemitStruct memory _remittance = remittances[secret];
+
+		// Validate if realSecret is correct
+		require(_remittance.remitSender != address(0));
+
+		// Validate if remittance has already been withdrawn
+		require(_remittance.remitBalance > 0);
+
+		// Validate that the deadline has passed and only remittance sender can request for refund
+		require(block.number > _remittance.deadline && msg.sender == _remittance.remitSender);
+
+		// Soft delete.
+		remittances[secret].remitBalance = 0;
+		LogRefund(msg.sender, _remittance.remitBalance, _remittance.deadline, secret);
+
+		// Interact with untrusted address last.
+		msg.sender.transfer(_remittance.remitBalance);
+	}
+
 
 	/*
 	 	Do not accept any funds from other sources.
